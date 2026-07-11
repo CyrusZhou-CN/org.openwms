d4360bb71cbea6920015474fae14e65f14977165
Heiko Scherrer <scherrer@openwms.org>
2026-07-11 13:49:12 +0200

    #185 Upgrade to Java 25 and SpringBoot 4.1.0

--- org.openwms.core.util/src/main/java/org/openwms/core/app/JSONConfiguration.java
+++ org.openwms.core.util/src/main/java/org/openwms/core/app/JSONConfiguration.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.ameba.annotation.ExcludeFromScan;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.jackson2.autoconfigure.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
